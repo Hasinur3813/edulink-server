70c3f2ea83335a907b55a37a1a852cd967165977
Hasinur <hasinur.rahman1332@gmail.com>
2024-12-24 15:46:42 +0600

    added assignment submission feature

--- routes/secureRoute.js
+++ routes/secureRoute.js
@@ -9,6 +9,7 @@ const secureRoute = express.Router();
 secureRoute.use(verifyToken);
 
 const assignmetCollection = database.collection("assignments");
+const submissionCollection = database.collection("submission");
 
 // create assignment
 secureRoute.post("/create", async (req, res, next) => {
@@ -43,7 +44,6 @@ secureRoute.get("/", async (req, res, next) => {
 });
 
 // delete assignment
-
 secureRoute.post("/delete/:id", async (req, res, next) => {
   try {
     const id = req.params.id;
@@ -54,6 +54,48 @@ secureRoute.post("/delete/:id", async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+});
+
+// get a single assignment
+secureRoute.post("/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const assignment = await assignmetCollection.findOne({
+      _id: new ObjectId(id),
+    });
+    if (!assignment) {
+      return res.status(404).send({
+        success: false,
+        message: "Assignment not found!",
+      });
+    }
+
+    res.send(assignment);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Submited assignment by users
+
+secureRoute.post("/submit-assignment", (req, res) => {
+  res.json(req.body);
+
+  // try {
+  //   const assignment = req.body;
+  //   console.log(assignment);
+  //   // const result = await submissionCollection.insertOne({
+  //   //   ...assignment,
+  //   //   date: new Date(assignment.date),
+  //   // });
+  //   // res.status(200).send({
+  //   //   success: true,
+  //   //   message: "Assignment submited successfully",
+  //   //   result,
+  //   // });
+  // } catch (error) {
+  //   next(error);
+  // }
 });
 
 export default secureRoute;
